585d18b0531e8a6e530105678a298f71c85f0e84
Jens472 <jens.swedish@gmail.com>
2018-05-26 19:44:18 +0200

    +Extension

---
+++ background.js
@@ -0,0 +1,11 @@
+if(window.location.href == "https://www.pornhub.com/") 
+    {
+		$("a.removeAdLink.removeAdsStyle").parents(':eq(2)').remove();
+	}
+$("a.removeAdLink.removeAdsStyle").parent().remove();
+$("a.ad-link.js-pop").parent().remove();
+$("ul.searchRelatedList.sectionContent.clearfix").parent().remove();
+$("ul.videos.row-4-thumbs.streamateContent.clearfix").parent().remove();
+$("div#welcome, div#PornhubNetworkBar, li#menuItem6, li#menuItem4, div.abovePlayer, div.streamatesModelsContainer, a#headerUpgradePremiumBtn, div.filters, li.becomeTranslator, li.getVerified, div.hd.clear, div#feed_block_, li.upgradeToPremium, div.reset, h2.footer-title, p.footer, section#footer, div.logoFooterWrapper").remove();
+//$("a.removeAdLink").parent().remove();
+//$("a.removeAdLink.removeAdsStyle").parent().remove();
